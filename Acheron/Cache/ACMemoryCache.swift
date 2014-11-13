fc8feafd5f1cf4459e0ef26a42fbda1f25480392
YANKE Guo <me@yanke.io>
2014-11-13 11:20:48 +0800

    working on memory cache

--- Acheron/Cache/ACMemoryCache.swift
+++ Acheron/Cache/ACMemoryCache.swift
@@ -11,6 +11,9 @@ import UIKit
 typealias ACMemoryCacheBlock        = (cache: ACMemoryCache) -> Void
 typealias ACMemoryCacheObjectBlock  = (cache: ACMemoryCache, key: String, object: AnyObject?) -> Void
 
+let ACMemoryCacheEmptyBlock       : ACMemoryCacheBlock        = { _     in return }
+let ACMemoryCacheObjectEmptyBlock : ACMemoryCacheObjectBlock  = { _,_,_ in return }
+
 let ACMemoryCachePrefix = "io.yanke.acmemorycache"
 
 var _sharedACMemoryCache : ACMemoryCache? = nil
@@ -27,8 +30,8 @@ class ACMemoryCache {
   var costLimit   : UInt  = 0
   var ageLimit    : NSTimeInterval = 0
   
-  var removeAllObjectsOnMemoryWarning       : Bool = false
-  var removeAllObjectsOnEnteringBackground  : Bool = false
+  var removeAllObjectsOnMemoryWarning       : Bool = true
+  var removeAllObjectsOnEnteringBackground  : Bool = true
   
   var willAddObjectBlock        : ACMemoryCacheObjectBlock?
   var willRemoveObjectBlock     : ACMemoryCacheObjectBlock?
@@ -49,63 +52,137 @@ class ACMemoryCache {
   init() {
     let queueName = "\(ACMemoryCachePrefix).\(CACurrentMediaTime())"
     self.queue = dispatch_queue_create(queueName, DISPATCH_QUEUE_CONCURRENT)
+    
+    //  Notifications
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "onNotificationReceived:", name: UIApplicationDidEnterBackgroundNotification, object: UIApplication.sharedApplication())
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "onNotificationReceived:", name: UIApplicationDidReceiveMemoryWarningNotification, object: UIApplication.sharedApplication())
   }
   
-  func objectForKey(key: String, block: ACMemoryCacheObjectBlock) {
+  func onNotificationReceived(notification: NSNotification) {
+    if notification.name == UIApplicationDidReceiveMemoryWarningNotification {
+      if self.removeAllObjectsOnMemoryWarning {
+        self.removeAllObjects(ACMemoryCacheEmptyBlock)
+      }
+      dispatch_async(self.queue, { [weak self] () -> Void in
+        if self == nil { return }
+        self?.didReceiveMemoryWarningBlock?(cache: self!)
+      })
+    } else if notification.name == UIApplicationDidEnterBackgroundNotification {
+      if self.removeAllObjectsOnEnteringBackground {
+        self.removeAllObjects(ACMemoryCacheEmptyBlock)
+      }
+      dispatch_async(self.queue, { [weak self] () -> Void in
+        if self == nil { return }
+        self?.didEnterBackgroundBlock?(cache: self!)
+      })
+    }
   }
   
-  func setObject(object: AnyObject?, forKey key: String, block: ACMemoryCacheObjectBlock?) {
+  //  MARK: - Internal Methods
+  
+  func removeObjectAndExecuteBlocksForKey(key: String) {
+    let obj : AnyObject? = self.dictionary.objectForKey(key)
+    self.willRemoveObjectBlock?(cache: self, key: key, object: obj)
+    if let cost = self.costs.objectForKey(key) as NSNumber? {
+      self.totoalCost -= cost.unsignedIntegerValue
+    }
+    self.dictionary.removeObjectForKey(key)
+    self.costs.removeObjectForKey(key)
+    self.dates.removeObjectForKey(key)
+    self.didRemoveObjectBlock?(cache: self, key: key, object: nil)
   }
   
-  func setObject(object: AnyObject?, forKey key: String, withCost cost: UInt, block: ACMemoryCacheObjectBlock) {
+  func _removeObjectForKey(key: String, block: ACMemoryCacheObjectBlock) {
+    dispatch_barrier_async(self.queue, { [weak self] () -> Void in
+      if self == nil { return }
+      self?.removeObjectAndExecuteBlocksForKey(key)
+      dispatch_async(self?.queue, { [weak self] () -> Void in
+        if self == nil { return }
+        block(cache: self!, key: key, object: nil)
+      })
+    })
   }
   
-  func removeObjectForKey(key: String, block: ACMemoryCacheObjectBlock) {
+  func _objectForKey(key: String, block: ACMemoryCacheObjectBlock) {
+    let now = NSDate()
+    dispatch_async(self.queue, { [weak self] () -> Void in
+      if self == nil { return }
+      var obj : AnyObject? = self?.dictionary.objectForKey(key)
+      if obj != nil {
+        dispatch_barrier_async(self?.queue, { [weak self] () -> Void in
+          if self == nil { return }
+          self?.dates.setObject(now, forKey: key)
+        })
+        block(cache: self!, key: key, object: obj)
+      }
+    })
   }
   
-  func trimToDate(date: NSDate, block: ACMemoryCacheBlock) {
+  func _setObject(object: AnyObject, forKey key: String, withCost cost: UInt, block: ACMemoryCacheObjectBlock) {
+    let now = NSDate()
+    dispatch_barrier_async(self.queue, { [weak self] () -> Void in
+      if self == nil { return }
+      self?.willAddObjectBlock?(cache: self!, key: key, object: object)
+      self?.dictionary.setObject(object, forKey: key)
+      self?.dates.setObject(now, forKey: key)
+      self?.costs.setObject(NSNumber(unsignedLong: cost), forKey: key)
+      self?.totoalCost += cost
+      self?.didAddObjectBlock?(cache: self!, key: key, object: object)
+      dispatch_async(self?.queue, { [weak self] () -> Void in
+        if self == nil { return }
+        block(cache: self!, key: key, object: object)
+      })
+    })
   }
   
-  func trimToCost(cost: UInt, block: ACMemoryCacheBlock) {
+  //  MARK: - External Methods
+  
+  func removeAllObjects(block: ACMemoryCacheBlock? = nil) {
   }
   
-  func trimToCostByDate(cost: UInt, block: ACMemoryCacheBlock) {
+  func objectForKey(key: String, block: ACMemoryCacheObjectBlock? = nil) -> AnyObject? {
+    if block != nil {
+      self._objectForKey(key, block: block!)
+      return nil
+    } else {
+      var obj : AnyObject? = nil
+      let sema = dispatch_semaphore_create(0)
+      self._objectForKey(key, block: { (cache, key, object) -> Void in
+        obj = object
+        dispatch_semaphore_signal(sema)
+      })
+      dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+      return obj
+    }
   }
   
-  func removeAllObjects(block: ACMemoryCacheBlock) {
+  func removeObjectForKey(key: String, block: ACMemoryCacheObjectBlock? = nil) {
+    if block != nil {
+      self._removeObjectForKey(key, block: block!)
+    } else {
+      let sema = dispatch_semaphore_create(0)
+      self._removeObjectForKey(key, block: { (cache, key, object) -> Void in
+        dispatch_semaphore_signal(sema); return
+      })
+      dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+    }
   }
   
-  func enumerateObjectsWithBlock(block: ACMemoryCacheObjectBlock, completionBlock: ACMemoryCacheBlock) {
+  func setObject(object: AnyObject?, forKey key: String, withCost cost: UInt, block: ACMemoryCacheObjectBlock? = nil) {
+    if object == nil {
+      self.removeObjectForKey(key, block: block)
+    } else {
+      if block != nil {
+        self._setObject(object!, forKey: key, withCost: cost, block: block!)
+      } else {
+        let sema = dispatch_semaphore_create(0)
+        self._setObject(object!, forKey: key, withCost: cost, block: { (cache, key, object) -> Void in
+          dispatch_semaphore_signal(sema)
+          return
+        })
+        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+      }
+    }
   }
   
-  //  MARK: - Sync Methods
-  
-  func objectForKey(key: String) -> AnyObject {
-    fatalError("Not finished")
-  }
-  
-  func setObject(object: AnyObject, forKey key: String) {
-  }
-  
-  func setObject(object: AnyObject, forKey key: String, withCost cost: UInt) {
-  }
-  
-  func removeObjectForKey(key: String) {
-  }
-  
-  func trimToDate(date: NSDate) {
-  }
-  
-  func trimToCost(cost: UInt) {
-  }
-  
-  func trimToCostByDate(cost: UInt) {
-  }
-  
-  func removeAllObjects() {
-  }
-  
-  func enumerateObjectsWithBlock(block: ACMemoryCacheObjectBlock) {
-  }
-   
 }
